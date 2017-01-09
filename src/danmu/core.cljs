7b42ae9ed946e27e86c3cf0ab9b7af8e251b00dd
Chen Li <chencassc@gmail.com>
2017-01-09 18:37:54 +0800

    animation with reanimated

--- src/danmu/core.cljs
+++ src/danmu/core.cljs
@@ -1,5 +1,6 @@
 (ns danmu.core
   (:require
+   [reanimated.core :as anim]
    [clojure.string :as s]
    [alandipert.storage-atom :refer [local-storage]]
    [reagent.core :as reagent :refer [atom]]
@@ -10,31 +11,60 @@
 (enable-console-print!)
 
 (defonce dm-items (local-storage (atom []) :dm-items))
+
+(defn gen-animate-item [item]
+  {:item item
+   :color (str "rgb(" (rand-int 255) "," (rand-int 255) "," (rand-int 255) ")")
+   :top (rand-nth (range 10 280))
+   :time (+ 5 (rand-int 15))})
+
+(defonce displayed-items (atom (map gen-animate-item @dm-items)))
 (defonce msg-store (atom nil))
 (defn add-item []
   (when-not (s/blank? @msg-store)
     (swap! dm-items conj @msg-store)
     (reset! msg-store nil)))
 
+(defn- -animated-item []
+  (fn [{:keys [item time top color]}]
+    [:div.animated-item
+     {:style
+      {:animation-duration (str time "s")
+       :top (str top "px")
+       :color color
+       ;; :animation-delay (str (rand-int 5) "s")
+       }}
+     item]))
+
 (defn animated-item []
-  (let [time (+ 4 (rand-int 20))
-        top (rand-nth (range 10 280))
-        color (str "rgb(" (rand-int 255) "," (rand-int 255) "," (rand-int 255) ")")]
-    (fn [item]
+  (let [cx (atom 0)]
+    (fn [{:keys [item time top color]}]
       [:div.animated-item
-       {:style
-        {:animation-duration (str time "s")
-         :top (str top "px")
-         :color color
-         ;; :animation-delay (str (rand-int 5) "s")
-         }}
-       item])))
+       [:svg
+        {:width 800
+         :height 300
+         ;;:color color
+         }
+        [anim/interval #(swap! cx inc) 10]
+        [:svg
+         [:text {:x @cx :y top} item]]]])))
+
+#_(let [cx (atom 0)
+      ;;cx (anim/interpolate-to x)
+      ]
+  (fn []
+    [:svg
+     {:width 560
+      :height 120}
+     [anim/interval #(swap! cx inc) 10]
+     [:svg
+      [:text {:x @cx :y 100} "hi"]]]))
 
 (defn display-dm-items []
   [:div.board
-   (for [item @dm-items]
+   (for [aitem @displayed-items]
      ^{:key (.random js/Math)}
-     [animated-item item])])
+     [animated-item aitem])])
 
 (defn my-app []
   (fn []
